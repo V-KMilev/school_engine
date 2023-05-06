be3db26e789591c12cbff6137ac0c2ac59c63d34
V-KMilev <vilimir.k.milev@gmail.com>
2023-05-06 23:04:19 +0300

    Add gets
    #1 get name
    #2 get function tree

--- src/function_handle/function_handle.cpp
+++ src/function_handle/function_handle.cpp
@@ -1,1 +1,9 @@
 #include "function_handle.h"
+
+const std::string& FunctionHandle::get_name() const {
+	return m_name;
+}
+
+const LogicalTree<std::string>& FunctionHandle::get_function_tree() const {
+	return m_function;
+}
--- src/function_handle/function_handle.h
+++ src/function_handle/function_handle.h
@@ -3,19 +3,21 @@
 #include "logical_tree.h"
 #include "string_array.h"
 
+#include "hash_map.h"
+
 class FunctionHandle {
 	public:
 		FunctionHandle() = default;
 		~FunctionHandle() = default;
 
-		virtual bool handle_params(const std::string &params) = 0;
+		const std::string& get_name() const;
 
-		virtual bool handle_body(const std::string &body) = 0;
+		const LogicalTree<std::string>& get_function_tree() const;
 
 	protected:
 		std::string m_name;
 
 		StringArray m_cached_params;
 
-		LogicalTree m_function;
+		LogicalTree<std::string> m_function;
 };
