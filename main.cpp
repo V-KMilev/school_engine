215ec8ce169f868aa09490ea0b8c9d91bce82af8
V-KMilev <vilimir.k.milev@gmail.com>
2023-05-07 19:01:48 +0300

    NIC

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
 #include "gl_render.h"
-#include "string_handle.h"
 
 #include "action_handle.h"
-
-#include "hash_map.h"
 #include "post.h"
 
 int main(int, char**) {
@@ -15,10 +12,23 @@ int main(int, char**) {
 	ActionHandle ah;
 
 	std::string inputTest1 = " DEFINE  testFunc1(a, b, c, d): \"a & (!b | !c)\"";
-	// std::string inputTest2 = " DEFINE  testFunc2(a, b, c, d): \"a & testFunc1(b,a,c,d)\"";
+	std::string inputTest2 = " DEFINE  testFunc2(a, b, c, d): \"a & (!b | !testFunc1(b,a,c,d))\"";
+	std::string inputTest3 = " DEFINE  testFunc3(a, b, c, d): \"testFunc2(b,a,c,d) & (!testFunc1(b,a,c,d) | !c)\"";
+	std::string inputTest4 = " DEFINE  testFunc4(a, b, c, d): \"c & testFunc3(b,a,c,d)\"";
 
 	ah.handle_input(inputTest1);
-	// ah.handle_input(inputTest2);
+
+	std::cout << "\n";
+
+	ah.handle_input(inputTest2);
+
+	std::cout << "\n";
+
+	ah.handle_input(inputTest3);
+
+	std::cout << "\n";
+
+	ah.handle_input(inputTest4);
 
 	std::cout << "\nHello, world!\n";
 }
