517828dcc358160213ac0f73a33eca6a707aa460
V-KMilev <vilimir.k.milev@gmail.com>
2023-05-08 01:53:08 +0300

    NIC

--- main.cpp
+++ main.cpp
@@ -11,24 +11,25 @@ int main(int, char**) {
 
 	ActionHandle ah;
 
-	std::string inputTest1 = " DEFINE  testFunc1(a, b, c, d): \"a & (!b | !c)\"";
-	std::string inputTest2 = " DEFINE  testFunc2(a, b, c, d): \"a & (!b | !testFunc1(b,a,c,d))\"";
-	std::string inputTest3 = " DEFINE  testFunc3(a, b, c, d): \"testFunc2(b,a,c,d) & (!testFunc1(b,a,c,d) | !c)\"";
-	std::string inputTest4 = " DEFINE  testFunc4(a, b, c, d): \"c & testFunc3(b,a,c,d)\"";
+	std::string inputTest1 = " DEFINE  testFunc1(a, b): \"a & b\"";
+	std::string inputTest2 = " DEFINE  testFunc2(a, b, c): \"a & (b | testFunc1(b,c))\"";
+	std::string inputTest3 = " DEFINE  testFunc3(a, b, c): \"testFunc2(c,b,a) & (testFunc1(a,c) | a)\"";
+	std::string inputTest4 = " DEFINE  testFunc4(a, b): \"b & testFunc3(b,a,d)\"";
 
 	ah.handle_input(inputTest1);
-
+	ah.get_logical_tree("testFunc1").print();
 	std::cout << "\n";
 
 	ah.handle_input(inputTest2);
-
+	ah.get_logical_tree("testFunc2").print();
 	std::cout << "\n";
 
 	ah.handle_input(inputTest3);
-
+	ah.get_logical_tree("testFunc3").print();
 	std::cout << "\n";
 
 	ah.handle_input(inputTest4);
+	ah.get_logical_tree("testFunc4").print();
 
 	std::cout << "\nHello, world!\n";
 }
